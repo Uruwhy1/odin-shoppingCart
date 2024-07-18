df31773b8795a782e9819d6de95a5021ffaf6ede
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-07-18 10:32:28 -0300

    add media queries to buy view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,20 +8,39 @@ import ModeToggle from "./ModeToggle";
 
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Header({ mode, setMode, setView }) {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setViewportWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const getImageSrc = () => {
+    if (viewportWidth <= 1000 && mode == "light") {
+      return storeLogoDark;
+    } else if (viewportWidth <= 1000 && mode == "dark") {
+      return storeLogoLight;
+    }
+    return mode === "light" ? storeLogoLight : storeLogoDark;
+  };
+
   return (
     <div className={styles.container + " header"}>
-      <ModeToggle mode={mode} setMode={setMode} />
       <div className={styles.content}>
         <Link to="/">
           <img
             className={styles.logo}
-            src={mode == "light" ? storeLogoLight : storeLogoDark}
+            src={getImageSrc()}
             alt="Store Logo"
             onClick={() => setView("buy")}
           />
         </Link>
+        <ModeToggle mode={mode} setMode={setMode} />
 
         <div className={styles.options}>
           <button onClick={() => setView("cart")} className={styles.cart}>
@@ -37,5 +56,5 @@ export default function Header({ mode, setMode, setView }) {
 Header.propTypes = {
   mode: PropTypes.string.isRequired,
   setMode: PropTypes.func.isRequired,
-  setView: PropTypes.func.isRequired
+  setView: PropTypes.func.isRequired,
 };
